425de37e96e39eea80be89c54e3062fe44bafa19
Mike <lvlichaelly@gmail.com>
2016-01-26 00:24:08 -0800

    uncommented devise user.rb methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
 
 # # the 2 lines below should be uncommented except during db:setup
 
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :group_members
   has_many :groups, through: :group_members

cf736ef0f813b517611ca5e17a9e27d29e320768
Mike <lvlichaelly@gmail.com>
2016-02-02 18:22:46 -0800

    edited navbar style

---
+++ app/assets/javascripts/components/eventsIndex/eventIndexPage.jsx
@@ -0,0 +1,10 @@
+var EventsIndexPage = React.createClass({
+
+  render: function() {
+    return (
+      <div className="events-index__container" >
+        <EventsIndex />
+      </div>
+    )
+  }
+});

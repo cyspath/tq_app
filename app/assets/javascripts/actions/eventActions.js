b8b9f98587a2e4eebe9453911c3fe880fc583976
Mike <lvlichaelly@gmail.com>
2016-02-11 18:34:44 -0800

    changed xhr request to plan jquery ajax calls in api.js

--- app/assets/javascripts/actions/eventActions.js
+++ app/assets/javascripts/actions/eventActions.js
@@ -1,19 +1,12 @@
 var EventActions = new _.extend({}, {
 
   getEvents: function(url) {
-    //   Api.get('/api/events').then(function(events) {
-    //     AppDispatcher.dispatch({
-    //       actionType: Constants.GET_EVENTS,
-    //       events: events
-    //     })
-    //   }.bind(this))
-      $.getJSON('/api/events' + url).then(function(events) {
-        AppDispatcher.dispatch({
-          actionType: Constants.GET_EVENTS,
-          events: events
-        })
-      }.bind(this))
-    // }
+    Api.get('/api/events' + url).then(function(events) {
+      AppDispatcher.dispatch({
+        actionType: Constants.GET_EVENTS,
+        events: events
+      })
+    }.bind(this))
   },
 
   getEventDetail: function(eventId) {
--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,44 +1,58 @@
 var Api = new _.extend({}, {
 
-  token: function() {
-    var el = document.querySelector('meta[name="csrf-token"]')
-    return el ? el.getAttribute('content') : "";
-  },
-
-  headers: function() {
-    return {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': this.token(),
-      'X-Requested-With': 'XMLHttpRequest'
-    }
-  },
-
   get: function(route, params) {
-    return this.xhr(route, params, 'get');
+    return this.serverAction(route, params, 'get');
   },
 
   put: function(route, params) {
-    return this.xhr(route, params, 'put');
+    return this.serverAction(route, params, 'put');
   },
 
   post: function(route, params) {
-    return this.xhr(route, params, 'post');
+    return this.serverAction(route, params, 'post');
   },
 
   delete: function(route, params) {
-    return this.xhr(route, params, 'delete');
+    return this.serverAction(route, params, 'delete');
   },
 
-
-  xhr: function(route, params, verb) {
-    return fetch(
-      route + '.json',
-      _.merge(
-        { method: verb, credentials: 'include', headers: this.headers() },
-        { body: JSON.stringify(params) }
-      )
-    ).then(function(resp) { return resp.json(); })
+  serverAction: function(route, params, verb) {
+    return $.ajax({
+      type: verb,
+      url: route,
+      data: params,
+      dataType: "json",
+      // success:function(resp){
+      //   console.log(resp);
+      //   return(resp)
+      // },
+      error:function(resp){
+        console.log(resp)
+      }
+    })
   }
 
+  // xhr: function(route, params, verb) {
+  //   return fetch(
+  //     route + '.json',
+  //     _.merge(
+  //       { method: verb, credentials: 'include', headers: this.headers() },
+  //       { body: JSON.stringify(params) }
+  //     )
+  //   ).then(function(resp) { return resp.json(); })
+  // },
+
+  // token: function() {
+  //   var el = document.querySelector('meta[name="csrf-token"]')
+  //   return el ? el.getAttribute('content') : "";
+  // },
+  //
+  // headers: function() {
+  //   return {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'X-CSRF-Token': this.token(),
+  //     'X-Requested-With': 'XMLHttpRequest'
+  //   }
+  // }
 });
